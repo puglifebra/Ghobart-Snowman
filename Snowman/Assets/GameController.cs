d7ebbeef5b9ce053f40e4ea78735268851c008c0
puglifebra <ghobart2024@chadwickschool.org>
2021-11-17 17:41:44 -0800

    work

--- Snowman/Assets/GameController.cs
+++ Snowman/Assets/GameController.cs
@@ -6,11 +6,19 @@ public class GameController : MonoBehaviour
 {
     public UnityEngine.UI.Text Message;
     public UnityEngine.UI.Button StartButton;
+    public UnityEngine.UI.Button BackButton;
 
     public void StartGame()
     {
         this.Message.text = "Can you save the Snowman?";
         this.StartButton.gameObject.SetActive(false);
-    }
+        this.BackButton.gameObject.SetActive(true);
+        OpenStartScreen();
 
+    }
+    public void Start();
+    {
+        this.StartButton.gameObject.SetActive(true);
+        this.BackButton.gameObject.SetActive(false);
+    }
 }
